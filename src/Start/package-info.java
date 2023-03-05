7252fd4a1d0a2c5dcb1509e3630b35c23983b787
BaraaLalagui <baraa.lalagui@ensia.edu.dz>
2023-03-05 16:31:15 +0100

    sasaas

---
+++ src/Start/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author Baraa
+ *
+ */
+package Start;
+
+public class Start{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World !");
+    }
+}
